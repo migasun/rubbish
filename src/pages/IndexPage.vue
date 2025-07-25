ba67a8354e4fda440ff7ebaadf31377a657966e1
Wang Chia Hsiang <5805505+migasun@users.noreply.github.com>
2025-07-25 23:13:35 +0800

    refactor: cleanup IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -128,46 +128,53 @@
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
-
-</script>
 <script setup>
-import {onBeforeMount, ref} from "vue";
+import { onBeforeMount, onBeforeUnmount, ref, computed, defineOptions } from 'vue'
 import axios from "axios";
 import { API_BASE_URL } from "src/boot/axios";
 // 參考 https://github.com/SmartCodeDavid/vue3-json-viewer/blob/master/readme_cn.md
 import { JsonViewer } from "vue3-json-source-viewer"
 import "vue3-json-source-viewer/dist/index.css"
 
+defineOptions({ name: 'IndexPage' })
+
 const unwrap = (v) => (v && typeof v === 'object' && '#text' in v) ? v['#text'] : v
+const mapUrl = (lon, lat) => `https://maps.nlsc.gov.tw/go/${lon}/${lat}/15/EMAP_B/DMAPS,ROAD`
 const data24 = ref({})
-const arrival24 = ref({})
 const arrival_point24 = ref({})
 const home_point24 = ref({})
-const arrival_map24 = ref("")
-const home_map24 = ref("")
+const arrival_map24 = computed(() => {
+  const lon = arrival_point24.value.longitude?.['#text'] ?? arrival_point24.value.longitude
+  const lat = arrival_point24.value.latitude?.['#text'] ?? arrival_point24.value.latitude
+  return mapUrl(lon, lat)
+})
 const isLate24 = ref(null);
-const data24placemap = ref({});
+const data24placemap = computed(() =>
+  data24.value.place ? `https://www.google.com/maps/place/${data24.value.place}` : ''
+)
 const data60 = ref({})
-const arrival60 = ref({})
 const arrival_point60 = ref({})
 const home_point60 = ref({})
-const arrival_map60 = ref("")
-const home_map60 = ref("")
+const arrival_map60 = computed(() => {
+  const lon = arrival_point60.value.longitude?.['#text'] ?? arrival_point60.value.longitude
+  const lat = arrival_point60.value.latitude?.['#text'] ?? arrival_point60.value.latitude
+  return mapUrl(lon, lat)
+})
 const isLate60 = ref(null);
-const data60placemap = ref({});
+const data60placemap = computed(() =>
+  data60.value.place ? `https://www.google.com/maps/place/${data60.value.place}` : ''
+)
 const inService = ref("有垃圾車");
+let intervalID
 onBeforeMount(()=>{
   console.log("onBeforeMount");
   loadData()
-  var timeoutID = window.setInterval(( () => {
+  intervalID = setInterval(() => {
     loadData()
-  } ), 30000);
+  }, 30000)
+})
+onBeforeUnmount(() => {
+  clearInterval(intervalID)
 })
 
 function refresh(done){
@@ -186,7 +193,6 @@ function refresh(done){
       data24.value = res.data.line
       const arrivalIdx24 = parseInt(data24.value.arrival?.['#text'] ?? data24.value.arrival)
       if(arrivalIdx24 > 0){
-        arrival24.value = arrivalIdx24
         arrival_point24.value = data24.value.points.point[arrivalIdx24-1];
       }
       data24.value.points.point.forEach(item => {
@@ -197,14 +203,6 @@ function refresh(done){
 
       inService.value = (home_point24.value.schedule === '本日無清運') ?  "本日無清運" : null;
       inService.value = (home_point24.value.schedule === '停止收運') ?  "停止收運" : inService.value;
-
-      data24placemap.value = "https://www.google.com/maps/place/"+data24.value.place
-      const aLon24 = arrival_point24.value.longitude?.['#text'] ?? arrival_point24.value.longitude
-      const aLat24 = arrival_point24.value.latitude?.['#text'] ?? arrival_point24.value.latitude
-      const hLon24 = home_point24.value.longitude?.['#text'] ?? home_point24.value.longitude
-      const hLat24 = home_point24.value.latitude?.['#text'] ?? home_point24.value.latitude
-      arrival_map24.value = "https://maps.nlsc.gov.tw/go/" + aLon24 + "/" + aLat24 + "/15/EMAP_B/DMAPS,ROAD";
-      home_map24.value = "https://maps.nlsc.gov.tw/go/" + hLon24 + "/" + hLat24 + "/15/EMAP_B/DMAPS,ROAD";
 
       isLate24.value =
         parseInt(arrival_point24.value.rank?.['#text'] ?? arrival_point24.value.rank) -
@@ -223,7 +221,6 @@ axios.get(API_BASE_URL + '?line60=true')
 
     const arrivalIdx60 = parseInt(data60.value.arrival?.['#text'] ?? data60.value.arrival)
     if(arrivalIdx60 > 0){
-      arrival60.value = arrivalIdx60
       arrival_point60.value = data60.value.points.point[arrivalIdx60-1];
     }
 
@@ -232,14 +229,6 @@ axios.get(API_BASE_URL + '?line60=true')
         home_point60.value = item;
       }
     });
-    data60placemap.value ="https://www.google.com/maps/place/"+data60.value.place
-    const aLon60 = arrival_point60.value.longitude?.['#text'] ?? arrival_point60.value.longitude
-    const aLat60 = arrival_point60.value.latitude?.['#text'] ?? arrival_point60.value.latitude
-    const hLon60 = home_point60.value.longitude?.['#text'] ?? home_point60.value.longitude
-    const hLat60 = home_point60.value.latitude?.['#text'] ?? home_point60.value.latitude
-    arrival_map60.value = "https://maps.nlsc.gov.tw/go/" + aLon60 + "/" + aLat60 + "/15/EMAP_B/DMAPS,ROAD";
-    home_map60.value = "https://maps.nlsc.gov.tw/go/" + hLon60 + "/" + hLat60 + "/15/EMAP_B/DMAPS,ROAD";
-
     isLate60.value =
       parseInt(arrival_point60.value.rank?.['#text'] ?? arrival_point60.value.rank) -
       parseInt(home_point60.value.rank?.['#text'] ?? home_point60.value.rank);
@@ -249,7 +238,6 @@ axios.get(API_BASE_URL + '?line60=true')
   })
  }
 
-const bar = ref(null)
 </script>
 <style>
 
